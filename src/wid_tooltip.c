b41ef423b74a9fb7b851c903a46a0a90f7600533
Neil McGill <goblinhack@gmail.com>
2014-01-20 09:49:25 -0500

    work in progress

--- src/wid_tooltip.c
+++ src/wid_tooltip.c
@@ -42,10 +42,10 @@ widp wid_tooltip (const char *text, float x, float y, fontp font)
     mx *= global_config.xscale;
     mx *= global_config.yscale;
 
-    if ((my >= (int32_t) ((global_config.video_gl_width / 5) * 2)) &&
-        (my <= (int32_t) ((global_config.video_gl_height / 5) * 4))) {
+    if ((my >= (int32_t) ((global_config.video_gl_width / 7) * 2)) &&
+        (my <= (int32_t) ((global_config.video_gl_height / 7) * 5))) {
         /*
-         * Mouse in middle
+         * Mouse at to 
          */
         y = 0.2;
     }
