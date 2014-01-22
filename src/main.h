b3f764e8cc2373a81c19de311491fca72f380f20
Neil McGill <goblinhack@gmail.com>
2014-01-22 07:55:16 -0500

    work in progress
    Former-commit-id: 0ff3c06d75c2ee79bb244d428ac49e150707a7a5
    Former-commit-id: 2eb0a9bf118b8d716b7ffc490c111f5ca80a6e0c

--- src/main.h
+++ src/main.h
@@ -347,6 +347,9 @@ struct config {
     int32_t sound_volume;
     int32_t music_volume;
     char name[PLAYER_NAME_LEN_MAX];
+    char server_name[PLAYER_NAME_LEN_MAX];
+    uint16_t server_max_players;
+    uint16_t server_port;
 };
 
 extern struct config global_config;
