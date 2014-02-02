c77e5c8ee706de7b24ab760e68389253e05aefae
Neil McGill <goblinhack@gmail.com>
2014-02-02 14:37:50 -0500

    work in progress

---
+++ src/wid_server_create.h
@@ -0,0 +1,14 @@
+/*
+ * Copyright (C) 2011 Neil McGill
+ *
+ * See the README file for license.
+ */
+
+#include <SDL_net.h>
+
+boolean wid_server_create_init(void);
+void wid_server_create_fini(void);
+void wid_server_create_hide(void);
+void wid_server_create_visible(void);
+void wid_server_create_redo(boolean soft_refresh);
+boolean wid_server_save_local_server(void);
