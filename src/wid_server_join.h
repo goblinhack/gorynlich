7b3d2072a47c20bd958483b941c948fb57119765
Neil McGill <goblinhack@gmail.com>
2014-02-02 14:37:50 -0500

    work in progress
    Former-commit-id: c77e5c8ee706de7b24ab760e68389253e05aefae
    Former-commit-id: 62fd63f1bd13e7f4dc2804aa905b7bebe34421c4

---
+++ src/wid_server_join.h
@@ -0,0 +1,14 @@
+/*
+ * Copyright (C) 2011 Neil McGill
+ *
+ * See the README file for license.
+ */
+
+#include <SDL_net.h>
+
+boolean wid_server_join_init(void);
+void wid_server_join_fini(void);
+void wid_server_join_hide(void);
+void wid_server_join_visible(void);
+void wid_server_join_redo(boolean soft_refresh);
+boolean wid_server_save_remote_server_list(void);
