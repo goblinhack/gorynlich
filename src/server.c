88b8415846ebb5eba44c5b76b9bfa456d71037ad
Neil McGill <goblinhack@gmail.com>
2014-02-22 14:28:00 -0500

    work in progress
    Former-commit-id: 188a2b08d9ab3d918ab79c916fccffc78848ec6b
    Former-commit-id: 96cb59c76d3f9ce6cbc34d15160f5499084deae6

--- src/server.c
+++ src/server.c
@@ -125,6 +125,8 @@ static void server_rx_client_join (socketp s)
     socket_tx_server_shout_except_to(tmp, s);
     myfree(tmp);
 
+    socket_tx_map_update(s);
+
     wid_game_map_server_visible();
 }
 
