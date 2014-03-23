22c788611cb19cd34c435eb2de68c47585868289
Neil McGill <goblinhack@gmail.com>
2014-03-22 20:35:32 -0400

    work in progress

--- src/config.h
+++ src/config.h
@@ -131,4 +131,5 @@
  * server. Set this too high and the client can push through walls. Too
  * low and it will be jerky, jumping back when we have high latency.
  */
-#define THING_MAX_SERVER_DISCREPANCY 1.0
+#define THING_MAX_SERVER_DISCREPANCY 2.0
+#define THING_PLAYER_POLL_SPEED      1 // hundredths
