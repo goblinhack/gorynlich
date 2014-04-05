4a9925a8149cadff65b5deb0a51be2ed7609180b
Neil McGill <goblinhack@gmail.com>
2014-04-05 18:15:51 -0400

    work in progress
    Former-commit-id: 44ef098ac6658a72bbecd3968e59d633bd364518
    Former-commit-id: 3f76d9e401d44033d8afe9a088db0f42deb236e0

--- src/config.h
+++ src/config.h
@@ -11,7 +11,7 @@
 #undef ENABLE_ASSERT              // DIE on errors
 #undef ENABLE_TREE_SANITY         // Slow tree sanity
 #undef ENABLE_DEBUG               // More debugs
-#undef ENABLE_VIDEO_SYNC         // Vertical sync
+#define ENABLE_VIDEO_SYNC         // Vertical sync
 #undef ENABLE_GENERATE_TTF        // Edit RUNME to add -lSDL_ttf
 #undef ENABLE_DEMARSHAL_DEBUG     // Demarshalling, prints structures
 #undef ENABLE_MAP_DEBUG           // Debug AI by printing the map
