a86a2b760180cbd9603afbbd8b253cb959c813d8
Neil McGill <goblinhack@gmail.com>
2014-06-01 12:22:06 -0400

    work in progress
    Former-commit-id: 2f14bfe17ffd0539c50a49292e6439b5c33d9e60
    Former-commit-id: 1484e2c1e807c5b3546bc7aaf8c5a3e926cca716

--- src/wid_notify.c
+++ src/wid_notify.c
@@ -12,6 +12,8 @@
 #include "ttf.h"
 #include "wid_notify.h"
 #include "time.h"
+#include "tree.h"
+#include "string_ext.h"
 
 static const int32_t PAD_X = 80;
 static const int32_t PAD_Y = 25;
@@ -30,6 +32,7 @@ static double Y = 1.0;
 static double H = 0.05;
 static uint32_t DELAY = 5000;
 
+#if 0
 /*
  * Create the wid_notify
  */
@@ -47,12 +50,82 @@ wid_notify_internal (const char *text, uint32_t level)
     {
         wid_notify_window = wid_new_rounded_window("wid_notify");
         wid_set_text(wid_notify_window, text);
-        wid_set_font(wid_notify_window, font);
-        wid_set_bevel(wid_notify_window, 1);
-        wid_set_rounded_small(wid_notify_window);
 
-        wid_set_color(wid_notify_window, WID_COLOR_TEXT, WHITE);
+    }
 
+    wid_raise(wid_notify_window);
+
+    wid_update(wid_notify_window);
+
+    wid_set_do_not_lower(wid_notify_window, true);
+
+    return (wid_notify_window);
+}
+#endif
+/*
+ * Create the wid_notify
+ */
+static widp
+wid_notify_internal (const char *text, uint32_t level)
+{
+    widp wid_notify_container;
+    widp wid_notify_window;
+    tree_string_split_node *n;
+    tree_root *d;
+    uint32_t maxw = 0;
+    uint32_t maxh = 0;
+    uint32_t toth = 0;
+    uint32_t rows = 0;
+    uint32_t chars_per_line;
+    uint32_t max_rows;
+
+    chars_per_line = 20;
+    max_rows = 22;
+
+    d = split(text, chars_per_line);
+
+    /*
+     * Get some rough sizes for the font.
+     */
+    { TREE_WALK(d, n) {
+        uint32_t w;
+        uint32_t h;
+
+        ttf_text_size(small_font, n->line, &w, &h, 0, 1.0f, 1.0f,
+                      false /* fixed width */);
+
+        /*
+         * .5 line spacing for single newlines.
+         */
+        if (!strlen(n->line)) {
+            h = maxh/2;
+        }
+
+        maxw = max(w, maxw);
+        maxh = max(h, maxh);
+
+        toth += h;
+        rows++;
+    } }
+
+    {
+        wid_notify_window = wid_new_rounded_window("wid_notify");
+
+        fpoint tl = {0, 0};
+        fpoint br = {0, 0};
+
+        br.x += maxw;
+
+        if (rows > max_rows) {
+            br.y += maxh * max_rows;
+        } else {
+            br.y += toth;
+        }
+
+        wid_set_tl_br(wid_notify_window, tl, br);
+    }
+
+    {
         color c = BLACK;
 
         switch (level) {
@@ -74,31 +147,75 @@ wid_notify_internal (const char *text, uint32_t level)
         c.a = 150;
         wid_set_color(wid_notify_window, WID_COLOR_TL, c);
         wid_set_color(wid_notify_window, WID_COLOR_BR, c);
-        wid_set_text_outline(wid_notify_window, true);
-
-        fpoint tl = {0, 0};
-        fpoint br = {0, 0};
-
-        tl.x = X;
-        tl.y = 0;
-
-        br.x = 1.0;
-        br.y = H;
-
-        wid_set_tl_br_pct(wid_notify_window, tl, br);
 
         wid_move_to_pct(wid_notify_window, X, 0);
     }
 
-    wid_fade_in(wid_notify_window, wid_fade_delay);
+    {
+        fpoint tl = {0, 0};
+        fpoint br = {0, 0};
 
-    wid_set_ignore_events(wid_notify_window, true);
+        br.x += maxw;
+
+        if (rows > max_rows) {
+            br.y += maxh * max_rows;
+        } else {
+            br.y += toth;
+        }
+
+        wid_notify_container = wid_new_container(wid_notify_window,
+                                                 "wid notify container2");
+        wid_set_tl_br(wid_notify_container, tl, br);
+    }
+
+    {
+        int32_t row = 0;
+        int32_t h = 0;
+        widp child;
+
+        TREE_WALK(d, n) {
+            fpoint tl = {0.0f, 0.0f};
+            fpoint br = {0.0f, 0.0f};
+
+            tl.x = 0;
+            tl.y = h;
+            br.x = maxw;
+            br.y = h + maxh;
+
+            row++;
+
+            /*
+             * .5 line spacing for single newlines.
+             */
+            if (!strlen(n->line)) {
+                h += maxh/2;
+            } else {
+                h += maxh;
+            }
+
+            child = wid_new_container(wid_notify_container,
+                                      "wid notify container3");
+            wid_set_tl_br(child, tl, br);
+            wid_set_text(child, n->line);
+            wid_set_text_lhs(child, true);
+            wid_set_font(child, vsmall_font);
+//            wid_set_text_outline(child, true);
+            wid_set_color(child, WID_COLOR_TEXT, WHITE);
+        }
+
+        split_free(&d);
+    }
+
+    wid_set_bevel(wid_notify_window, 1);
+    wid_set_rounded_small(wid_notify_window);
 
     wid_raise(wid_notify_window);
 
-    wid_update(wid_notify_window);
-
     wid_set_do_not_lower(wid_notify_window, true);
+
+    wid_fade_in(wid_notify_window, wid_fade_delay);
+
+    wid_set_ignore_events(wid_notify_window, true);
 
     return (wid_notify_window);
 }
@@ -174,7 +291,7 @@ void wid_notify_tick (void)
         wid_notify("there", NORMAL);
         wid_notify("you", NORMAL);
         wid_notify("oh-oh", WARNING);
-        wid_notify("panic panic dive dive!", SEVERE);
+        wid_notify("panic panic panic panic panic panic v panic v v panic panic panic v panicx x x x x x x x x x x x x x x x x x x panic dive dive!", SEVERE);
         x = 1;
     }
 
