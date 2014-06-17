3db7acf7d3c1ec16771220f7ea64f314bd811c9b
Neil McGill <goblinhack@gmail.com>
2014-06-17 06:58:36 -0400

    work in progress
    Former-commit-id: 41ecd6b304a7907c9068147a9dae112c8f185787
    Former-commit-id: 39331bfc49e91d2ce11e65d9864b1f08ba793ba9

--- src/config.h
+++ src/config.h
@@ -152,7 +152,6 @@
  */
 #define DELAY_TENTHS_THING_AI                   2
 #define DELAY_TENTHS_THING_COLLISION_TEST       1
-#define DELAY_TENTHS_THING_FIRE                 3
 #define DELAY_TENTHS_PING                       5
 #define DELAY_TENTHS_SOCKET_QUALITY_CHECK       10
 #define DELAY_TENTHS_CURSOR_VISIBLE             5
