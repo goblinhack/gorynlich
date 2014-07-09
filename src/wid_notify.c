440910267629fc78a9c861f401abe1bef2746dfb
Neil McGill <goblinhack@gmail.com>
2014-07-09 07:01:26 -0400

    work in progress

--- src/wid_notify.c
+++ src/wid_notify.c
@@ -116,14 +116,6 @@ wid_notify_internal (const char *text, uint32_t level)
             bg = DARKRED;
             fg = RED;
             break;
-        case POPUP:
-            bg = DARKRED;
-            fg = RED;
-
-            widp w = wid_popup_simple(text);
-            wid_move_to_pct_centered(w, 0.5, -1);
-            wid_move_to_pct_centered_in(w, 0.5, 0.2, 2 * ONESEC);
-            wid_destroy_in(w, ONESEC * 3);
             break;
 
         }
