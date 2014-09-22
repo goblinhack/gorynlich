c28ece73a17f2727138ff9e98f95a8df092f5f04
Neil McGill <goblinhack@gmail.com>
2014-09-22 08:35:52 -0400

    work in progress
    Former-commit-id: 18e120b5819f14f471908675fdc9e53eb10ef695
    Former-commit-id: 350d594b78b0a46782d1e0ff6354a6ae3dff8c0f

--- src/wid_intro_about.c
+++ src/wid_intro_about.c
@@ -112,11 +112,11 @@ static void wid_intro_about_create (void)
           ,
           "%%fg=red$Stuffs.",       /* title */
           0.5, 0.5,                 /* x,y postition in percent */
-          small_font,              /* title font */
+          small_font,               /* title font */
           vsmall_font,              /* body font */
-          small_font,              /* button font */
+          vsmall_font,              /* button font */
           1,                        /* number buttons */
-          "Close", wid_intro_about_callback_close);
+          "  Close  ", wid_intro_about_callback_close);
 
     wid_set_on_key_down(wid_intro_about, wid_intro_about_key_event);
 
