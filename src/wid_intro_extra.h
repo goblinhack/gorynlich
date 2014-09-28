d3c8690e2a37dbb00e157465376382d285a7085f
Neil McGill <goblinhack@gmail.com>
2014-09-28 10:14:24 -0400

    work in progress
    Former-commit-id: 000e93d86f2b278590713cb593eb91ab93dc55ee
    Former-commit-id: 9d390c1d08a8ff63125fe5c7bc024048ee85c7d8

---
+++ src/wid_intro_extra.h
@@ -0,0 +1,10 @@
+/*
+ * Copyright (C) 2011 Neil McGill
+ *
+ * See the LICENSE file for license.
+ */
+
+uint8_t wid_intro_extra_init(void);
+void wid_intro_extra_fini(void);
+void wid_intro_extra_hide(void);
+void wid_intro_extra_visible(void);
