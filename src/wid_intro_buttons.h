ee7a9d99458e35905035164bb8544fc6af3f0b32
Neil McGill <goblinhack@gmail.com>
2014-09-28 21:18:34 -0400

    work in progress

---
+++ src/wid_intro_buttons.h
@@ -0,0 +1,10 @@
+/*
+ * Copyright (C) 2011 Neil McGill
+ *
+ * See the LICENSE file for license.
+ */
+
+void wid_intro_buttons_hide(void);
+void wid_intro_buttons_visible(void);
+void wid_intro_intro_extra_selected(void);
+uint8_t wid_intro_quit_selected(void);
