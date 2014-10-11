e715c717209a7a9b4e8c7cae369600a16fbbf423
Neil McGill <goblinhack@gmail.com>
2014-10-10 18:49:48 -0700

    work in progress
    Former-commit-id: 609ca3217b44d3db91cca102b1e09c6d442d03e8
    Former-commit-id: 32ef54401da769a11a5d9298c7f32620fcc33e77

---
+++ data/music/oggconv.sh
@@ -0,0 +1,6 @@
+for i in DST_Alters.wav DST_AncientCurse_II.wav DST_Arch_Delerium.wav DST_Arches.wav DST_BattleLands.wav DST_BlackRiver.wav DST_BoneTower.wav DST_ClockTower.wav DST_DarkestKnight.wav DST_DoomCity.wav DST_ExInfernus.wav DST_LeDanse.wav DST_Legends.wav DST_SuperWin.wav charlotte.wav charlotte8bit.wav charlotte_slow.wav
+do
+f=`echo $i | sed 's/\..*//g'`
+echo $f
+oggenc -q 10 $f.wav -o $f.ogg
+done
