271cd1df531ac14fb35d3b2a0f1b6b3f891fc34f
Neil McGill <goblinhack@gmail.com>
2014-10-19 19:28:55 -0400

    work in progress
    Former-commit-id: 7eef984ae4eefe8aa307c774110838efacfe1e34
    Former-commit-id: 1ac159a30b3116c43465192866c87796eee693d8

--- src/wid_game_map_client.c
+++ src/wid_game_map_client.c
@@ -1092,7 +1092,7 @@ void wid_game_map_client_score_update (levelp level, uint8_t redo)
                         char *full_tooltip;
                         char *tmp = strappend(tooltip, "\n\n");
 
-                        if (thing_template_is_click_to_use(temp)) {
+                        if (thing_template_is_rrr22(temp)) {
                             char *old = tmp;
                             tmp = strappend(
                                     old,
@@ -1100,7 +1100,7 @@ void wid_game_map_client_score_update (levelp level, uint8_t redo)
                             myfree(old);
                         }
 
-                        if (thing_template_is_click_to_drop(temp)) {
+                        if (thing_template_is_rrr23(temp)) {
                             char *old = tmp;
                             tmp = strappend(
                                     old,
