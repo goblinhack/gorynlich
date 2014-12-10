66fa3c9c729fd24ed30f7ae79c228792099bbb46
Neil McGill <goblinhack@gmail.com>
2014-12-10 16:07:07 -0500

    work in progress
    Former-commit-id: 63bcbc742e186553a305418a828dba0a0a986be2
    Former-commit-id: 340b011aa791250f50803ba92b8dde6fd67e8e37

--- installer/windows.sh
+++ installer/windows.sh
@@ -1,4 +1,4 @@
 "/c/Program Files (x86)/BitRock InstallBuilder Enterprise 9.5.3/bin/builder-cli.exe" build windows.xml 
 
-cp /c/Users/nmcgill/Documents/InstallBuilder/output/gorynlich-0.01-windows-installer.exe .
+cp /c/Users/nmcgill/Documents/InstallBuilder/output/gorynlich-0.01-windows-installer.exe ../gorynlich-installer.exe
 
