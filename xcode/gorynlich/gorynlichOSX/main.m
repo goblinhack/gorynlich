1a4864a0e19db48575894bd8b77dcd4b3e608cf0
Neil McGill <goblinhack@gmail.com>
2014-12-15 19:35:47 -0500

    work in progress
    Former-commit-id: 7a79d422cbc76f5d2e1bd5086e7507d6fb65d20a
    Former-commit-id: 95a53c6c6b414478e9feb88a2511a82140ae3b5f

---
+++ xcode/gorynlich/gorynlichOSX/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  gorynlichOSX
+//
+//  Created by Neil McGill on 12/14/14.
+//  Copyright (c) 2014 Neil McGill. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
