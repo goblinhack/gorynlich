b20508a6356a4839c24538187031ce79b4ed27c0
Neil McGill <goblinhack@gmail.com>
2015-01-19 09:58:40 -0500

    work in progress
    Former-commit-id: 0e81746a77bb944b12d4edab3dd97ea55cacee08
    Former-commit-id: 65184abef83a3de9bb53d919279e557be3fa01d0

--- src/config.h
+++ src/config.h
@@ -161,7 +161,6 @@
  * Fonts
  */
 #undef ENABLE_GENERATE_TTF         // Edit RUNME to add -lSDL_ttf
-#define ENABLE_GENERATE_TTF         // Edit RUNME to add -lSDL_ttf
 
 #define FIXED_FONT                  "data/ttf/emulogic.ttf"
 #if 0
