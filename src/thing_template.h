bbcd30ce199c153c4d55bc9fa3af09a8fdeacae3
Neil McGill <goblinhack@gmail.com>
2015-01-29 07:27:14 -0500

    work in progress
    Former-commit-id: 5b4c6f9756587f79736267806b2ee8cf3261e3dc
    Former-commit-id: 44cb636ca7d42096937d24906ce5ab1f4f9d89ba

--- src/thing_template.h
+++ src/thing_template.h
@@ -226,7 +226,7 @@ typedef struct tp_ {
     uint8_t is_rrr3:1;
     uint8_t is_rrr4:1;
     uint8_t can_walk_through:1;
-    uint8_t is_weapon_carry_effect:1;
+    uint8_t is_weapon_carry_anim:1;
     uint8_t is_spell:1;
     uint8_t is_hand_item:1;
     uint8_t is_boots:1;
@@ -539,9 +539,9 @@ static inline uint8_t tp_can_walk_through (tpp t)
     return (t->can_walk_through);
 }
 
-static inline uint8_t tp_is_weapon_carry_effect (tpp t)
+static inline uint8_t tp_is_weapon_carry_anim (tpp t)
 {
-    return (t->is_weapon_carry_effect);
+    return (t->is_weapon_carry_anim);
 }
 
 static inline uint8_t tp_is_spell (tpp t)
