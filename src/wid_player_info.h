daee1c52d0b8e07a973c5a4ce625750156e30ca4
Neil McGill <goblinhack@gmail.com>
2015-01-31 17:14:12 -0800

    work in progress
    Former-commit-id: 03b909e48467ab7cf5a0d19b6516bd301ccdc3bf
    Former-commit-id: 758f4b7ca69f80c2d36099108164f9ad69f52382

--- src/wid_player_info.h
+++ src/wid_player_info.h
@@ -7,5 +7,6 @@
 uint8_t wid_player_info_init(void);
 void wid_player_info_fini(void);
 void wid_player_info_hide(void);
-void wid_player_info_visible(thing_statsp );
+void wid_player_info_visible(thing_statsp);
+int wid_player_info_is_visible(void);
 extern int wid_player_info_set_name;
