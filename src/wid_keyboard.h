e9be06570b06427f219dff2bd9b509d293152c65
Neil McGill <goblinhack@gmail.com>
2015-02-14 21:16:47 -0500

    work in progress
    Former-commit-id: 69aa6ff57e9224b56af16ed3eeee4c56a18d5d45
    Former-commit-id: c9bc1f68af858a80f1399bf5294093504457ee0a

---
+++ src/wid_keyboard.h
@@ -0,0 +1,59 @@
+/*
+ * Copyright (C) 2011 Neil McGill
+ *
+ * See the README file for license.
+ */
+
+#pragma once
+
+typedef void(*wid_keyboard_event_t)(widp, const char *text);
+
+#define WID_KEYBOARD_ACROSS 14
+#define WID_KEYBOARD_DOWN   5
+
+typedef struct {
+    /*
+     * Parent widget
+     */
+    widp w;
+
+    /*
+     * Text input widget
+     */
+    widp input;
+
+    /*
+     * Entered text.
+     */
+    char text[MAXSTR];
+
+    /*
+     * Item currently in focus
+     */
+    int focusx;
+    int focusy;
+
+    /*
+     * When the keyboard was made.
+     */
+    uint32_t created;
+
+    /*
+     * Items in the keyboard
+     */
+    widp buttons[WID_KEYBOARD_DOWN][WID_KEYBOARD_ACROSS];
+
+    /*
+     * What to call on events
+     */
+    wid_keyboard_event_t selected;
+    wid_keyboard_event_t cancelled;
+
+} wid_keyboard_ctx;
+
+widp wid_keyboard (const char *text,
+                   const char *title,
+                   wid_keyboard_event_t selected,
+                   wid_keyboard_event_t cancelled);
+
+extern int wid_keyboard_visible;
