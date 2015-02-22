1aefb25249cc1e92f96b8cf772786806516bacf1
Neil McGill <goblinhack@gmail.com>
2015-02-22 15:59:44 -0500

    work in progress
    Former-commit-id: 538cef122149406f482d40cec388910b10c15ac6
    Former-commit-id: 04d9ff905029f5d212f934c1510f3336e2336714

---
+++ src/wid_choose_pclass.h
@@ -0,0 +1,10 @@
+/*
+ * Copyright (C) 2011 Neil McGill
+ *
+ * See the LICENSE file for license.
+ */
+
+uint8_t wid_choose_pclass_init(void);
+void wid_choose_pclass_fini(void);
+void wid_choose_pclass_hide(void);
+void wid_choose_pclass_visible(void);
