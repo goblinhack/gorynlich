538cef122149406f482d40cec388910b10c15ac6
Neil McGill <goblinhack@gmail.com>
2015-02-22 15:59:44 -0500

    work in progress

--- src/wid_choose_name.c
+++ src/wid_choose_name.c
@@ -11,6 +11,7 @@
 #include "wid_choose_game_type.h"
 #include "wid_intro.h"
 #include "wid_server_join.h"
+#include "wid_keyboard.h"
 #include "wid_server_create.h"
 #include "timer.h"
 #include "glapi.h"
@@ -18,87 +19,87 @@
 #include "wid_menu.h"
 #include "name.h"
 
-static widp wid_choose_pclass_type;
-static widp wid_choose_pclass_type_background;
+static widp wid_choose_name;
+static widp wid_choose_name_background;
 
-static uint8_t wid_choose_pclass_type_init_done;
-static void wid_choose_pclass_type_create(void);
-static widp menu;
+static uint8_t wid_choose_name_init_done;
+static void wid_choose_name_create(void);
+static widp menu_keyboard;
 
 static int intro_effect_delay = 200;
 
-uint8_t wid_choose_pclass_type_init (void)
+uint8_t wid_choose_name_init (void)
 {
-    if (!wid_choose_pclass_type_init_done) {
+    if (!wid_choose_name_init_done) {
     }
 
-    wid_choose_pclass_type_init_done = true;
+    wid_choose_name_init_done = true;
 
     return (true);
 }
 
-void wid_choose_pclass_type_fini (void)
+void wid_choose_name_fini (void)
 {
     FINI_LOG("%s", __FUNCTION__);
 
-    if (wid_choose_pclass_type_init_done) {
-        wid_choose_pclass_type_init_done = false;
+    if (wid_choose_name_init_done) {
+        wid_choose_name_init_done = false;
 
-        if (wid_choose_pclass_type) {
-            wid_destroy(&wid_choose_pclass_type);
-            wid_destroy_in(wid_choose_pclass_type_background, wid_hide_delay * 2);
-            wid_choose_pclass_type_background = 0;
+        if (wid_choose_name) {
+            wid_destroy(&wid_choose_name);
+            wid_destroy_in(wid_choose_name_background, wid_hide_delay * 2);
+            wid_choose_name_background = 0;
         }
 
-        if (menu) {
-            wid_destroy(&menu);
+        if (menu_keyboard) {
+            wid_destroy(&menu_keyboard);
         }
     }
 }
 
-static uint8_t wid_choose_pclass_type_is_hidden;
-static uint8_t wid_choose_pclass_type_is_visible;
+static uint8_t wid_choose_name_is_hidden;
+static uint8_t wid_choose_name_is_visible;
 
-void wid_choose_pclass_type_hide (void)
+void wid_choose_name_hide (void)
 {
-    if (wid_choose_pclass_type_is_hidden) {
+    if (wid_choose_name_is_hidden) {
         return;
     }
 
-    wid_choose_pclass_type_is_hidden = true;
-    wid_choose_pclass_type_is_visible = false;
+    wid_choose_name_is_hidden = true;
+    wid_choose_name_is_visible = false;
 
-    if (!wid_choose_pclass_type) {
-        DIE("no wid intro");
+    if (!wid_choose_name) {
+        return;
     }
 
-    wid_fade_out(wid_choose_pclass_type_background, intro_effect_delay);
+    wid_fade_out(wid_choose_name_background, intro_effect_delay);
 
-    wid_hide(wid_choose_pclass_type, 0);
-    wid_raise(wid_choose_pclass_type);
-    wid_update(wid_choose_pclass_type);
+    wid_hide(wid_choose_name, 0);
+    wid_raise(wid_choose_name);
+    wid_update(wid_choose_name);
 
-    wid_destroy(&wid_choose_pclass_type);
-    wid_destroy_in(wid_choose_pclass_type_background, wid_hide_delay * 2);
-    wid_choose_pclass_type_background = 0;
+    wid_destroy(&wid_choose_name);
+    wid_destroy_in(wid_choose_name_background, wid_hide_delay * 2);
+    wid_choose_name_background = 0;
 
-    if (menu) {
-        wid_destroy(&menu);
+    if (menu_keyboard) {
+        wid_destroy(&menu_keyboard);
     }
 }
 
-void wid_choose_pclass_type_visible (void)
+void wid_choose_name_visible (void)
 {
-    if (wid_choose_pclass_type_is_visible) {
+    if (wid_choose_name_is_visible) {
         return;
     }
 
-    wid_choose_pclass_type_create();
+    wid_choose_name_create();
 
-    wid_choose_pclass_type_is_visible = true;
-    wid_choose_pclass_type_is_hidden = false;
+    wid_choose_name_is_visible = true;
+    wid_choose_name_is_hidden = false;
 
-    if (!wid_choose_pclass_type) {
+    if (!wid_choose_name) {
         DIE("no wid intro");
     }
 
@@ -107,35 +108,33 @@ void wid_choose_pclass_type_visible (void)
         return;
     }
 
-    wid_visible(wid_choose_pclass_type, 0);
-    wid_raise(wid_choose_pclass_type);
-    wid_update(wid_choose_pclass_type);
+    wid_visible(wid_choose_name, 0);
+    wid_raise(wid_choose_name);
+    wid_update(wid_choose_name);
 
-    wid_fade_in(wid_choose_pclass_type_background, intro_effect_delay);
+    wid_fade_in(wid_choose_name_background, intro_effect_delay);
 }
 
-static void wid_choose_pclass (void *context)
+static void wid_keyboard_event_selected (widp w, const char *text)
 {
-    wid_choose_pclass_type_hide();
+    thing_statsp s = &global_config.stats;
+    strncpy(s->pname, text, sizeof(s->pname) - 1);
+
+    wid_choose_name_hide();
     wid_choose_game_type_visible();
 }
 
-static uint8_t 
-wid_choose_pclass_go_back (void)
+static void wid_keyboard_event_cancelled (widp w, const char *text)
 {
-    wid_choose_pclass_type_hide();
+    wid_choose_name_hide();
     wid_intro_visible();
-
-    return (true);
 }
 
-static uint8_t wid_choose_pclass_type_play_key_event (widp w, const SDL_KEYSYM *key)
+static uint8_t wid_choose_name_play_key_event (widp w, const SDL_KEYSYM *key)
 {
     switch (key->sym) {
-        case 'b':
-        case 'q':
         case SDLK_ESCAPE:
-            wid_choose_pclass_type_hide();
+            wid_choose_name_hide();
             wid_intro_visible();
             return (true);
 
@@ -146,16 +145,16 @@ static uint8_t wid_choose_pclass_type_play_key_event (widp w, const SDL_KEYSYM *
     return (false);
 }
 
-static void wid_choose_pclass_type_bg_create (void)
+static void wid_choose_name_bg_create (void)
 {
     widp wid;
 
-    if (wid_choose_pclass_type_background) {
+    if (wid_choose_name_background) {
         return;
     }
 
     {
-        wid = wid_choose_pclass_type_background = wid_new_window("bg");
+        wid = wid_choose_name_background = wid_new_window("bg");
 
         float f = (1024.0 / 680.0);
 
@@ -179,60 +178,44 @@ static void wid_choose_pclass_type_bg_create (void)
     }
 }
 
-static void wid_choose_pclass_type_create (void)
+static void wid_choose_name_create (void)
 {
-    if (wid_choose_pclass_type) {
+    if (wid_choose_name) {
         return;
     }
 
-    wid_choose_pclass_type = wid_new_window("intro buttons");
+    wid_choose_name = wid_new_window("intro buttons");
 
-    wid_set_no_shape(wid_choose_pclass_type);
+    wid_set_no_shape(wid_choose_name);
 
     fpoint tl = {0.0f, 0.0f};
     fpoint br = {1.0f, 1.0f};
-    wid_set_tl_br_pct(wid_choose_pclass_type, tl, br);
-    wid_set_on_key_down(wid_choose_pclass_type, wid_choose_pclass_type_play_key_event);
+    wid_set_tl_br_pct(wid_choose_name, tl, br);
+    wid_set_on_key_down(wid_choose_name, wid_choose_name_play_key_event);
 
     color col = BLACK;
     col.a = 0;
     glcolor(col);
 
-    wid_set_mode(wid_choose_pclass_type, WID_MODE_NORMAL);
-    wid_set_color(wid_choose_pclass_type, WID_COLOR_TL, col);
-    wid_set_color(wid_choose_pclass_type, WID_COLOR_BR, col);
-    wid_set_color(wid_choose_pclass_type, WID_COLOR_BG, col);
+    wid_set_mode(wid_choose_name, WID_MODE_NORMAL);
+    wid_set_color(wid_choose_name, WID_COLOR_TL, col);
+    wid_set_color(wid_choose_name, WID_COLOR_BR, col);
+    wid_set_color(wid_choose_name, WID_COLOR_BG, col);
 
-    wid_choose_pclass_type_bg_create();
-    wid_update(wid_choose_pclass_type);
+    wid_choose_name_bg_create();
+    wid_update(wid_choose_name);
 
-    wid_move_to_pct_centered(wid_choose_pclass_type, 0.5f, 0.5f);
+    wid_move_to_pct_centered(wid_choose_name, 0.5f, 0.5f);
 
-    thing_statsp s;
-    s = &global_config.stats;
-
-    int focus;
-    const char *current_pclass = s->pclass;
-    int n = pclass_find(current_pclass);
-    if (n != -1) {
-        focus = n;
-    } else {
-        focus = pclass_count() / 2;
+    /*
+     * If there is an existing pclass then use that and set the focus to it.
+     */
+    thing_statsp s = &global_config.stats;
+    if (!strlen(s->pname)) {
+        strncpy(s->pname, name_random(s->pclass), sizeof(s->pname) - 1);
     }
 
-    menu = wid_menu(0,
-                 vvlarge_font,
-                 large_font,
-                 0.5, /* x */
-                 0.7, /* y */
-                 0.95, /* padding between buttons */
-                 focus, /* focus */
-                 pclass_count() + 1, /* items */
-
-                 (int) 'z', "wizard",   wid_choose_pclass,
-                 (int) 'd', "dwarf",    wid_choose_pclass,
-                 (int) 'w', "warrior",  wid_choose_pclass,
-                 (int) 'v', "valkyrie", wid_choose_pclass,
-                 (int) 'e', "elf",      wid_choose_pclass,
-                 (int) 'b', "back",     wid_choose_pclass_go_back);
+    menu_keyboard = wid_keyboard(s->pname, "Choose a name",
+                                 wid_keyboard_event_selected,
+                                 wid_keyboard_event_cancelled);
 }
