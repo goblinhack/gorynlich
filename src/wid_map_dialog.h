e7f0ca9868884a726755072d3f32b634e6c038a9
Neil McGill <goblinhack@gmail.com>
2015-03-08 11:47:36 -0400

    work in progress
    Former-commit-id: 51a5087b9367c3c259c0068aa0f3a74245227262
    Former-commit-id: 50d3b407e97836c3f5dc8c320aa7e58c9c1b5ed5

---
+++ src/wid_map_dialog.h
@@ -0,0 +1,10 @@
+/*
+ * Copyright (C) 2011 Neil McGill
+ *
+ * See the LICENSE file for license.
+ */
+
+uint8_t wid_map_dialog_init(void);
+void wid_map_dialog_fini(void);
+void wid_map_dialog_hide(void);
+void wid_map_dialog_visible(void);
