dedb10fb07c329b9d998a6f219c8cb1c8d2920a4
Neil McGill <goblinhack@gmail.com>
2015-04-18 17:25:34 -0400

    work in progress
    Former-commit-id: adb41fe057fb14209f0370cca7e14856c141048e
    Former-commit-id: 3953834b8d1e693ab857e547e844c0f227b7ceb2

--- src/thing_tick.c
+++ src/thing_tick.c
@@ -248,25 +248,9 @@ void thing_tick_server_player_slow_all (int force)
         thing_sanity(t);
 
         /*
-         * If something changed in the player that we need to update the 
-         * server, do so now.
+         * Count our torches.
          */
-        if (force || time_have_x_secs_passed_since(60, t->timestamp_torch)) {
-            t->timestamp_torch = time_get_time_ms();
-
-            /*
-             * Use up one torch unit.
-             */
-            tpp tp = thing_is_carrying_thing(t, tp_is_torch);
-            if (tp) {
-                thing_used(t, tp);
-                tp = thing_is_carrying_thing(t, tp_is_torch);
-                if (!tp) {
-                    MSG_SERVER_SHOUT_AT_PLAYER(INFO, t,
-                                               "Your light fizzles out");
-                }
-            }
-        }
+        thing_torch_update_count(t, force);
 
         /*
          * If health went over the max, tick it down.
@@ -301,17 +285,7 @@ void thing_tick_server_player_slow_all (int force)
              * Work out the torch light radius. Each torch lights 0.5 radius 
              * units.
              */
-            float torch_light_radius = 
-                (double) thing_is_carrying_thing_count(t, tp_is_torch) / 2.0;
-            if (torch_light_radius > 
-                tp_get_light_radius(t->tp)) {
-                torch_light_radius = tp_get_light_radius(t->tp);
-            }
-
-            if (torch_light_radius != t->torch_light_radius) {
-                t->torch_light_radius = torch_light_radius;
-                thing_update(t);
-            }
+            thing_torch_calculate_light(t);
         }
     }
 }
