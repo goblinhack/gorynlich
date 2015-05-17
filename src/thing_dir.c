fcd1c6f8a0362b5a39a6c2c56f731eb99a59bc6e
Neil McGill <goblinhack@gmail.com>
2015-05-17 16:15:22 -0400

    work in progress
    Former-commit-id: 2e835816a9db5c1aff249712cd1a23a052cbd416
    Former-commit-id: 637edf459b0e629d955be90cf4ca6c8c6f3ee3bd

---
+++ src/thing_dir.c
@@ -0,0 +1,83 @@
+/*
+ * Copyright (C) 2011 Neil McGill
+ *
+ * See the LICENSE file for license.
+ */
+
+#include "main.h"
+#include "thing.h"
+#include "thing_stats.h"
+#include "string_ext.h"
+#include "math_util.h"
+
+void thing_dir (thingp t, double *dx, double *dy)
+{
+    *dx = 0;
+    *dy = 0;
+
+    if (thing_is_dir_down(t)) {
+        *dy = 1.0;
+    }
+
+    if (thing_is_dir_up(t)) {
+        *dy = -1.0;
+    }
+
+    if (thing_is_dir_right(t)) {
+        *dx = 1.0;
+    }
+
+    if (thing_is_dir_left(t)) {
+        *dx = -1.0;
+    }
+
+    if (thing_is_dir_tl(t)) {
+        *dx = -1.0;
+        *dy = -1.0;
+    }
+
+    if (thing_is_dir_tr(t)) {
+        *dx = 1.0;
+        *dy = -1.0;
+    }
+
+    if (thing_is_dir_bl(t)) {
+        *dx = -1.0;
+        *dy = 1.0;
+    }
+
+    if (thing_is_dir_br(t)) {
+        *dx = 1.0;
+        *dy = 1.0;
+    }
+}
+
+int thing_angle_to_dir (double dx, double dy)
+{
+    fpoint r;
+    r.x = dx;
+    r.y = -dy;
+
+    int segment = ((int)(round(radians2angle(anglerot(r)) / 45.0))) % 8;
+
+    switch (segment) {
+        case 2:
+            return (THING_DIR_UP);
+        case 1:
+            return (THING_DIR_TR);
+        case 0:
+            return (THING_DIR_RIGHT);
+        case 7:
+            return (THING_DIR_BR);
+        case 6:
+            return (THING_DIR_DOWN);
+        case 5:
+            return (THING_DIR_BL);
+        case 4:
+            return (THING_DIR_LEFT);
+        case 3:
+            return (THING_DIR_TL);
+    }
+
+    DIE("thing_angle_to_dir bug");
+}
