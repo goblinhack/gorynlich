f7325b27c9337cbbd09ba0c62cbe6111477aab6e
Neil McGill <goblinhack@gmail.com>
2015-05-17 16:19:54 -0400

    work in progress
    Former-commit-id: 0dbbde7316e13b61d87aa347c3777e6209a35058
    Former-commit-id: 881f89b3fbd88d38d795e3ee0b652f87c14de094

--- src/thing_bomb.c
+++ src/thing_bomb.c
@@ -4,17 +4,9 @@
  * See the LICENSE file for license.
  */
 
-// REMOVED #include <SDL.h>
-// REMOVED #include "slre.h"
 
 #include "main.h"
-// REMOVED #include "math_util.h"
-// REMOVED #include "level.h"
 #include "thing.h"
-// REMOVED #include "wid_game_map_server.h"
-// REMOVED #include "map.h"
-// REMOVED #include "vision.h"
-// REMOVED #include "wid.h"
 
 thingp level_place_bomb (levelp level, 
                          thingp owner,
