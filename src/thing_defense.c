18f7c1564e4ec1f63a87d8d40f7bd6a9b9309565
Neil McGill <goblinhack@gmail.com>
2015-05-23 20:49:52 -0400

    work in progress
    Former-commit-id: b25661361b32603e31521905a1359377ba03c89a
    Former-commit-id: 189c1a592ffdf78ab358d85a6466f0be50ad72ad

--- src/thing_defense.c
+++ src/thing_defense.c
@@ -28,18 +28,21 @@ int32_t thing_stats_get_total_damage_minus_defense (thingp t,
     double modifier = thing_stats_val_to_modifier(thing_stats_get_defense(t));
 
     if (modifier < 0) {
-        d += d / -modifier;
+        d *= -(modifier - 1.0);
     } else if (modifier == 0) {
         /*
          * No change.
          */
     } else {
-        d -= d / modifier;
+        d /= (modifier + 0.5);
     }
 
     if (d < 1) {
         d = 1;
     }
+
+    THING_LOG(t, "damage modified from %d to %d, modifier %f", 
+              damage, (int)d, modifier);
 
     return ((int)d);
 }
