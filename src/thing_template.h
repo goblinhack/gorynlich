cb0143a850f81771b36e9b83f6a6e3f7dd89daa7
Neil McGill <goblinhack@gmail.com>
2015-06-21 12:59:08 -0400

    work in progress
    Former-commit-id: 48d411a63ebbb8f3de98c377ec0d337823958479
    Former-commit-id: 30b16e61da7b8222d847de11af3ddbab65fc2174

--- src/thing_template.h
+++ src/thing_template.h
@@ -119,6 +119,11 @@ typedef struct tp_ {
     char *sound_create;
 
     /*
+     * Sound on hitting a thing
+     */
+    char *sound_hitting;
+
+    /*
      * How this weapon appears on screen when it is used.
      */
     char *weapon_carry_anim;
@@ -422,6 +427,7 @@ color tp_light_color(tpp);
 const char *tp_spawn_on_death(tpp);
 const char *tp_explodes_as(tpp);
 const char *tp_sound_create(tpp);
+const char *tp_sound_hitting(tpp);
 const char *tp_weapon_carry_anim(tpp);
 const char *tp_shield_carry_anim(tpp);
 const char *tp_magic_anim(tpp);
