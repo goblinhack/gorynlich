2b0914de4a131bdb9a5d094f5b6c215cf8ab93ec
Neil McGill <goblinhack@gmail.com>
2015-06-20 22:08:33 -0400

    work in progress
    Former-commit-id: 0df282ed177595ebef684d84d430934631196ef5
    Former-commit-id: e707119041fd21e51b96311608a56843a3850b2a

--- src/thing_fire.c
+++ src/thing_fire.c
@@ -485,8 +485,10 @@ LOG("  owner");
 
     if (thing_is_shopkeeper(me)) {
         if (thing_is_monst(it)) {
-            thing_possible_hit_add(me, it);
-            return;
+            if (!thing_is_shopkeeper(it)) {
+                thing_possible_hit_add(me, it);
+                return;
+            }
         }
 
         if (thing_is_angry(me)) {
