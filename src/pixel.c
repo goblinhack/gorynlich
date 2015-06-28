28d031ba8653de47f56dc88ab838ef01e010cfed
Neil McGill <goblinhack@gmail.com>
2015-06-28 13:29:29 -0400

    work in progress
    Former-commit-id: 0ca359d82e0776143d2ec7106bd80ed993a3fda6
    Former-commit-id: 5732efa16187c1740cc438af81788ede0a42303d

--- src/pixel.c
+++ src/pixel.c
@@ -1,25 +1,8 @@
 /*
-    Retro.
-    Copyright (C) 1999, 2000 Neil McGill
-
-    This game is free software; you can redistribute it and/or
-    modify it under the terms of the GNU Library General Public
-    License as published by the Free Software Foundation; either
-    version 2 of the License, or (at your option) any later version.
-
-    This game is distributed in the hope that it will be fun,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-    Library General Public License for more details.
-
-    You should have received a copy of the GNU Library General Public
-    License along with this game; if not, write to the Free
-    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
-
-    Neil McGill
-
-    $Id: pixel.c 1037 2012-08-22 11:55:24Z 4670 $
-*/
+ * Copyright (C) 2011-2015 goblinhack@gmail.com
+ *
+ * See the LICENSE file for license.
+ */
 
 #include <SDL.h>
 
