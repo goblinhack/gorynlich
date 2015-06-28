42b8e528e902b4404b862b92260806800e4cecd6
Neil McGill <goblinhack@gmail.com>
2015-06-28 13:29:29 -0400

    work in progress
    Former-commit-id: d35dd397a8b4cf3dc97d154db38c8675b8425cee
    Former-commit-id: 92ee00705234c17c95cc54bc04fd665e38778c59

--- src/ptrcheck.h
+++ src/ptrcheck.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2011 Neil McGill
+ * Copyright (C) 2011-2015 goblinhack@gmail.com
  *
  * See the README file for license.
  * A useful hack to help debug stale/free/bad pointers.
