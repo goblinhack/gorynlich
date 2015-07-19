d18547aba8f6147960fe8ce943278f1231d52d6d
Neil McGill <goblinhack@gmail.com>
2015-07-18 22:04:19 -0400

    work in progress
    Former-commit-id: 28c6b5ac2511e0948703557d4322fc71c2183786
    Former-commit-id: 86df40a872283b88496348ecc49f1bf8e5f2e27f

--- src/music.h
+++ src/music.h
@@ -12,6 +12,7 @@ musicp music_load(const char *file, const char *name);
 musicp music_find(const char *name);
 void music_play(const char *file, const char *alias, uint32_t rate);
 void music_play_demo(void);
+void music_play_death(void);
 void music_play_intro(void);
 void music_play_game(level_pos_t);
 void music_play_dead(void);
