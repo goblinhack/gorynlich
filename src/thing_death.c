141ec13162717c1a71046c2f45fb9ea709e33f30
Neil McGill <goblinhack@gmail.com>
2015-07-22 21:42:41 -0400

    work in progress
    Former-commit-id: 95b34de17833432b57a07be27b2c8ec26ab1f8cd
    Former-commit-id: 6e67f5a67f42200e523691e6b7f02e578263d86f

--- src/thing_death.c
+++ src/thing_death.c
@@ -70,7 +70,7 @@ void thing_death_tick (void)
     }
 
     if (!level_death_is_coming_soon(server_level)) {
-        if (time_have_x_secs_passed_since(240,
+        if (time_have_x_secs_passed_since(120,
                                           level_get_timestamp_started(server_level))) {
             MSG_SERVER_SHOUT_AT_ALL_PLAYERS(POPUP, 
                                             0, 0, 
@@ -81,7 +81,7 @@ void thing_death_tick (void)
     }
 
     if (!level_death_is_coming(server_level)) {
-        if (time_have_x_secs_passed_since(300,
+        if (time_have_x_secs_passed_since(150,
                                           level_get_timestamp_started(server_level))) {
             MSG_SERVER_SHOUT_AT_ALL_PLAYERS(POPUP, 
                                             0, 0,
