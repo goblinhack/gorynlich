96bd7df5b357b39c787d70202878f987ac35aa22
Neil McGill <goblinhack@gmail.com>
2015-08-01 17:59:14 -0400

    work in progress
    Former-commit-id: f201577ffeba3cf39e99fde388ae97c54226caa2
    Former-commit-id: 444253ce59a04c6186c18c5fc1a6a8f451053a45

--- src/wid_intro.c
+++ src/wid_intro.c
@@ -600,7 +600,7 @@ static void wid_intro_menu_create (void)
 
     {
         widp w = wid_popup(
-                  "Version 0.0.1 alpha dive dive",
+                  "Version 0.0.2 alpha dive dive",
                   "",               /* title */
                   0.9f, 0.95f,      /* x,y postition in percent */
                   0,                /* title font */
