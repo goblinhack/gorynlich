d7e62491e732c65cc4e635ac04fa7efeee1c6cf8
Neil McGill <goblinhack@gmail.com>
2015-09-09 22:22:11 -0400

    work in progress
    Former-commit-id: 053d1d53831677aef702c2aef19a56b796453e99
    Former-commit-id: 4b46fdd8ec8dee03acdd6a6c013edabf261a064d

--- src/pixel.h
+++ src/pixel.h
@@ -96,13 +96,176 @@
 #define getPixel_32bpp(s, x, y, rgb)        *(rgb) = *pixel_32bpp(s, x, y)
 
 /*
- * Public interface. Clips to surface and performs locking. Slow.
+ * Lock a SDL_Surface for safe color access
  */
-void putPixel(SDL_Surface *dst, uint32_t x, uint32_t, color pix);
+static inline void lock (SDL_Surface *surface)
+{
+#if 0
+    /*
+     * Is this needed? unlocking seems slow
+     */
+    if (SDL_MUSTLOCK(surface)) {
+        SDL_LockSurface(surface);
+    }
+#endif
+}
 
 /*
- * Public interface. Clips to surface and performs locking. Slow.
+ * Unlock a SDL_Surface, after lock()
  */
-color getPixel(SDL_Surface *dst, uint32_t x, uint32_t y);
+static inline void unlock (SDL_Surface *surface)
+{
+#if 0
+    /*
+     * Is this needed? unlocking seems slow
+     */
+    if (SDL_MUSTLOCK(surface)) {
+        SDL_UnlockSurface(surface);
+    }
+#endif
+}
 
+/*
+ * This function sets the specified color on a SDL_Surface. Sanity checks are
+ * performed on the co-ordinates and the SDL_Surface is locked for you.
+ * Safe, but slow. For more speed, try the lower level access function.
+ */
+static void inline putPixel (SDL_Surface * surface, uint32_t x, uint32_t y, color col)
+{
+    uint8_t   bpp = surface->format->BytesPerPixel;
+    uint16_t pitch = surface->pitch/bpp;
+    uint32_t rgb;
+
+    if (x >= (uint32_t)surface->w) {
+        DIE("out of range x %u, max %u", x, surface->w);
+    }
+
+    if (y >= (uint32_t)surface->h) {
+        DIE("out of range y %u, max %u", y, surface->h);
+    }
+
+    lock(surface);
+
+#if SDL_BYTEORDER == SDL_BIG_ENDIAN
+    rgb = ((col.r << 24) | (col.g << 16) | (col.b << 8) | col.a);
+#else
+    rgb = (col.r | (col.g << 8) | (col.b << 16) | (col.a << 24));
+#endif
+
+    switch (pitch) {
+        case 16:
+            putPixel_16x32bpp(surface, x, y, rgb);
+            break;
+        case 24:
+            putPixel_24x32bpp(surface, x, y, rgb);
+            break;
+        case 32:
+            putPixel_32x32bpp(surface, x, y, rgb);
+            break;
+        case 48:
+            putPixel_48x32bpp(surface, x, y, rgb);
+            break;
+        case 64:
+            putPixel_64x32bpp(surface, x, y, rgb);
+            break;
+        case 128:
+            putPixel_128x32bpp(surface, x, y, rgb);
+            break;
+        case 256:
+            putPixel_256x32bpp(surface, x, y, rgb);
+            break;
+        case 320:
+            putPixel_320x32bpp(surface, x, y, rgb);
+            break;
+        case 640:
+            putPixel_640x32bpp(surface, x, y, rgb);
+            break;
+        case 800:
+            putPixel_800x32bpp(surface, x, y, rgb);
+            break;
+        case 1024:
+            putPixel_1024x32bpp(surface, x, y, rgb);
+            break;
+        default:
+            putPixel_32bpp(surface, x, y, rgb);
+            break;
+    }
+
+    unlock(surface);
+}
+
+/*
+ * This function sets the specified color on a SDL_Surface. Sanity checks are
+ * performed on the co-ordinates and the SDL_Surface is locked for you.
+ * Safe, but slow. For more speed, try the lower level access function.
+ */
+static inline color getPixel (SDL_Surface * surface, uint32_t x, uint32_t y)
+{
+    uint8_t   bpp = surface->format->BytesPerPixel;
+    uint16_t pitch = surface->pitch/bpp;
+    uint32_t rgb;
+    color     col;
+
+    if ((x >= (uint32_t)surface->w) || (y >= (uint32_t)surface->h)) {
+        DIE("getPixel out of range, pix %d,%d in size %d,%d",
+            x, y, surface->w, surface->h);
+    }
+
+    lock(surface);
+
+    switch (pitch) {
+        case 16:
+            getPixel_16x32bpp(surface, x, y, &rgb);
+            break;
+        case 24:
+            getPixel_24x32bpp(surface, x, y, &rgb);
+            break;
+        case 32:
+            getPixel_32x32bpp(surface, x, y, &rgb);
+            break;
+        case 48:
+            getPixel_48x32bpp(surface, x, y, &rgb);
+            break;
+        case 64:
+            getPixel_64x32bpp(surface, x, y, &rgb);
+            break;
+        case 128:
+            getPixel_128x32bpp(surface, x, y, &rgb);
+            break;
+        case 256:
+            getPixel_256x32bpp(surface, x, y, &rgb);
+            break;
+        case 320:
+            getPixel_320x32bpp(surface, x, y, &rgb);
+            break;
+        case 640:
+            getPixel_640x32bpp(surface, x, y, &rgb);
+            break;
+        case 800:
+            getPixel_800x32bpp(surface, x, y, &rgb);
+            break;
+        case 1024:
+            getPixel_1024x32bpp(surface, x, y, &rgb);
+            break;
+        default:
+            getPixel_32bpp(surface, x, y, &rgb);
+            break;
+    }
+
+#if SDL_BYTEORDER == SDL_BIG_ENDIAN
+    col.r = (rgb & 0xff000000) >> 24;
+    col.g = (rgb & 0x00ff0000) >> 16;
+    col.b = (rgb & 0x0000ff00) >> 8;
+    col.a = (rgb & 0x000000ff);
+#else
+    col.r = (rgb & 0x000000ff);
+    col.g = (rgb & 0x0000ff00) >> 8;
+    col.b = (rgb & 0x00ff0000) >> 16;
+    col.a = (rgb & 0xff000000) >> 24;
+#endif
+
+    unlock(surface);
+
+    return (col);
+}
 #endif
